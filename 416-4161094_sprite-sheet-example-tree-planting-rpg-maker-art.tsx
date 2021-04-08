b4c595306a749146639efddcd06cca9f268a3306
BadHorsie <82107372+BadHorsie@users.noreply.github.com>
2021-04-08 09:46:59 +0000

    Deploying to gh-pages from master @ 18ef500b049ef55c092ce49d219844118fe19650 🚀

---
+++ 416-4161094_sprite-sheet-example-tree-planting-rpg-maker-art.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="416-4161094_sprite-sheet-example-tree-planting-rpg-maker-art" tilewidth="32" tileheight="32" tilecount="250" columns="25">
+ <image source="416-4161094_sprite-sheet-example-tree-planting-rpg-maker-art.tif" trans="ffffff" width="820" height="322"/>
+</tileset>
---
+++ cars.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="cars" tilewidth="32" tileheight="32" tilecount="54" columns="9">
+ <image source="cars.tif" trans="ffffff" width="288" height="192"/>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.5455" y="9.27166"/>
+   <object id="2" x="14.5455" y="23.0516"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.2903" y="16.0766"/>
+   <object id="2" x="14.2903" y="8.33599"/>
+   <object id="3" x="9.95215" y="7.48538"/>
+   <object id="4" x="6.54971" y="16.0766"/>
+  </objectgroup>
+ </tile>
+</tileset>

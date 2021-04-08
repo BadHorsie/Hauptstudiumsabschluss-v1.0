c6fa3f5170c6a927de65162d7c20ecb515a56d9a
BadHorsie <82107372+BadHorsie@users.noreply.github.com>
2021-04-08 11:58:13 +0200

    sdfsdfgsg

---
+++ haus test.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="haus test" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="../Tiled Tilesets/haus test .png" width="192" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="3" duration="1000"/>
+   <frame tileid="4" duration="1000"/>
+   <frame tileid="5" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="1000"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="3" duration="1000"/>
+   <frame tileid="4" duration="1000"/>
+   <frame tileid="5" duration="1000"/>
+   <frame tileid="0" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="3" duration="1000"/>
+   <frame tileid="4" duration="1000"/>
+   <frame tileid="5" duration="1000"/>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="1000"/>
+   <frame tileid="7" duration="1000"/>
+   <frame tileid="8" duration="1000"/>
+   <frame tileid="9" duration="1000"/>
+   <frame tileid="10" duration="1000"/>
+   <frame tileid="11" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="1000"/>
+   <frame tileid="8" duration="1000"/>
+   <frame tileid="9" duration="1000"/>
+   <frame tileid="10" duration="1000"/>
+   <frame tileid="11" duration="1000"/>
+   <frame tileid="6" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="1000"/>
+   <frame tileid="9" duration="1000"/>
+   <frame tileid="10" duration="1000"/>
+   <frame tileid="11" duration="1000"/>
+   <frame tileid="6" duration="1000"/>
+   <frame tileid="7" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="1000"/>
+   <frame tileid="13" duration="1000"/>
+   <frame tileid="14" duration="1000"/>
+   <frame tileid="15" duration="1000"/>
+   <frame tileid="16" duration="1000"/>
+   <frame tileid="17" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="1000"/>
+   <frame tileid="14" duration="1000"/>
+   <frame tileid="15" duration="1000"/>
+   <frame tileid="16" duration="1000"/>
+   <frame tileid="17" duration="1000"/>
+   <frame tileid="12" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="1000"/>
+   <frame tileid="15" duration="1000"/>
+   <frame tileid="16" duration="1000"/>
+   <frame tileid="17" duration="1000"/>
+   <frame tileid="12" duration="1000"/>
+   <frame tileid="13" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ wände.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="wände" tilewidth="32" tileheight="32" tilecount="208" columns="8">
+ <image source="../Tiled Tilesets/wände.gif" width="256" height="832"/>
+</tileset>
